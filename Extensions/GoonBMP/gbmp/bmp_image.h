7374062b081e0cb1a9611ec5f9b2ab91a21dc6db
snowapril <sinjihng@naver.com>
2019-09-14 11:38:24 +0000

    bmp image io extension implement ongoing, how to set settings for travis-ci will be studied next time.

--- Extensions/GoonBMP/gbmp/bmp_image.h
+++ Extensions/GoonBMP/gbmp/bmp_image.h
@@ -1,1 +1,22 @@
 // reference on https://en.wikipedia.org/wiki/BMP_file_format 
+
+#include <fstream>
+
+namespace gbmp
+{
+    #pragma pack(push, 2)
+    struct bmp_file_header
+    {                                       // offset
+        unsigned short header_field;        //   0
+        unsigned int   size_of_bmp_file;    //   2
+        unsigned short reserved_1;          //   6
+        unsigned short reserved_2;          //   8
+        unsigned int   offset_pixel_start;  //   10
+    };
+    #pragma pack(pop)
+    
+    unsigned char* gbmp_load_image(char const* _image_path, unsigned int* _width, unsigned int* _height, unsigned int* _num_channels)
+    {
+        
+    }
+};
---
+++ GoonRenderer/Sources/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "This is test for checking build-status" << std::endl;
+    
+    return 0;
+}
